740810180c9fc5687282f5ea567fce153dab3f82
netsus <57648890+netsus@users.noreply.github.com>
2021-01-11 00:53:01 +0900

    #2.1 Reusable Components withd JSX + Props
    1. react magic : props -> Component 에서 Component로 값 전달.
    2. 함수에 argument 전달 할때 props.fav == {fav}

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
 import React from "react"
-import Potato from './Potato';
 
+function Food({fav}){
+  console.log(fav);
+  return <h1>I like {fav}</h1>
+}
 
 function App() {
   return (
   <div>
     <h1>Hello</h1>
-    <Potato />
+    <Food fav="kimchi" />
+    <Food fav="samgyupsal" />
+    <Food fav="ramyun" />
+    <Food fav="bulgogi" />
   </div>
   );
 }
