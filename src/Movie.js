a4308fb000c0d600ded30f091fe04edb5785b7ef
netsus <57648890+netsus@users.noreply.github.com>
2021-01-31 20:11:08 +0900

    #4.3 Adding Genres (1ver)
    1. JSX에서 HTML쓸때
    class 대신 className을 써주는게 좋다. 왜냐하면 Js내에서 class를 사용하기 때문에 react가 헷갈려할 수 있기 때문.

--- src/Movie.js
+++ src/Movie.js
@@ -3,14 +3,20 @@ import PropTyeps from "prop-types";
 import "./Movie.css";
 
 
-function Movie({ year, title, summary, poster}){
+function Movie({ year, title, summary, poster, genres}){
     return (
-    <div class="movie">
+    <div className="movie">
     <img src={poster} alt={title} title={title}></img>
-     <div class="movie__data">
-        <h3 class="movie__title">{title}</h3>
-        <h5 class="movie__year">{year}</h5>
-        <p class="movie__summary">{summary}</p>
+     <div className="movie__data">
+        <h3 className="movie__title">{title}</h3>
+        <h5 className="movie__year">{year}</h5>
+        <ul className="movie__genres">
+            {genres.map((genre, index) => 
+            <li key={index} className="genres__genre">
+                {genre}
+            </li>)}
+        </ul>
+        <p className="movie__summary">{summary}</p>
      </div>
     </div>
     )
@@ -22,6 +28,7 @@ Movie.propTypes = {
     title: PropTyeps.string.isRequired,
     summary: PropTyeps.string.isRequired,
     poster: PropTyeps.string.isRequired,
+    genres: PropTyeps.arrayOf(PropTyeps.string).isRequired
 }
 
 export default Movie;
