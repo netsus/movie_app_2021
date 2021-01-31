d65d0a106b4a3090107ae98c5a44a2edb1f4c8fb
netsus <57648890+netsus@users.noreply.github.com>
2021-01-31 18:36:13 +0900

    #4.0 Fetching Movies from API
    1. fetch 함수
      API 사용할때 네트워크의 json 같은 파일 가져오는 함수
    2. axios
    fetch위에 있는 작은 layer
    
    3. async 함수 -> await
    특정 함수가 실행될 시간을 기다렸다가 다음 행동들을 할 수 있도록하는 함수

--- src/App.js
+++ src/App.js
@@ -1,15 +1,17 @@
 import React from "react";
-import PropTypes from "prop-types";
+import axios from "axios";
 
 class App extends React.Component{
   state = {
     isLoading: true
   };
+
+
+  getMoives = async () => {
+    const movies = await axios.get("https://yts-proxy.now.sh/list_movies.json")
+  }
   componentDidMount() {
-    setTimeout(
-      () => {this.setState({isLoading : false});
-    },3000
-    )
+    this.getMoives();
   }
   render(){
     const {isLoading} = this.state;
