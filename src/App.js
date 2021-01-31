31344f031e6eca769c339e4aa049f42695827725
netsus <57648890+netsus@users.noreply.github.com>
2021-01-31 16:31:36 +0900

    #3.2 Component Life Cycle
    1. Component Life Cycle
    react가 component를 생성하는 그리고 없애는 방법이다.
     1) Mounting : Component의 생성
     2) Updating : Component의 업데이트
     3) Unmounting : Component의 죽음
    
    2. LifeCycle 순서
     1) 실행 - Render Mount
     2) setState - Render Update
     3) 페이지 나갈 때 - Umnount
    
    ** react component에서 사용하는 유일한 function은 render function.

--- src/App.js
+++ src/App.js
@@ -2,6 +2,10 @@ import React from "react";
 import PropTypes from "prop-types";
 
 class App extends React.Component{
+  constructor(props) {
+    super(props);
+    console.log("hello cons");
+  }
   state = {
     count:0
   };
@@ -13,7 +17,14 @@ class App extends React.Component{
     console.log("minus");
     this.setState( current => ({count: current.count - 1}));
   };
+  componentDidMount(){
+    console.log("component rendered");
+  }
+  componentDidUpdate(){
+    console.log("I jus updated");
+  }
   render(){
+    console.log("render")
     return (
     <div>
       <h1>The number is: {this.state.count}</h1>
