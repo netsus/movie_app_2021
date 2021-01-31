d6136555f2026f88db61e301eb2444e2e6a6925e
netsus <57648890+netsus@users.noreply.github.com>
2021-01-31 17:09:09 +0900

    #3.3 Planning the Movie Component
    1. state에 변수를 선언하는것은 필수가 아니다. 즉, setState할때 변수를 선언해도 된다. 하지만 처음 state를 정의할때 변수들을 모두 선언하는것이 좋은습관.

--- src/App.js
+++ src/App.js
@@ -2,36 +2,18 @@ import React from "react";
 import PropTypes from "prop-types";
 
 class App extends React.Component{
-  constructor(props) {
-    super(props);
-    console.log("hello cons");
-  }
   state = {
-    count:0
+    isLoading: true
   };
-  add = () => {
-    console.log("add");
-    this.setState( current => ({count: current.count + 1}));
-  };
-  minus = () => {
-    console.log("minus");
-    this.setState( current => ({count: current.count - 1}));
-  };
-  componentDidMount(){
-    console.log("component rendered");
-  }
-  componentDidUpdate(){
-    console.log("I jus updated");
+  componentDidMount() {
+    setTimeout(
+      () => {this.setState({isLoading : false});
+    },3000
+    )
   }
   render(){
-    console.log("render")
-    return (
-    <div>
-      <h1>The number is: {this.state.count}</h1>
-      <button onClick={this.add}>Add</button>
-      <button onClick={this.minus}>Minus</button>
-    </div>
-    );
+    const {isLoading} = this.state;
+    return <div> {isLoading ? "Loading..." : "We are ready"}</div>
   }
 }
 
